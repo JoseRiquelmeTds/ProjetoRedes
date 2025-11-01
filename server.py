12fae5433927d0f603f82d82225f25210505e8f0
Guilherme Novais <166192149+GuilhermeANovais@users.noreply.github.com>
2025-10-31 22:05:32 -0300

    Implement message history and locking mechanism
    Added message history management and thread safety.

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ HOST = '0.0.0.0'
 PORT = 5005
 
 clients = []
+message_history = [] #pra armazenar o historico das 10 msg
+history_lock = threading.lock() #pra garantir que o historico não vai ser modificado por duas threads at the same time
 
 def send_message(message, source_socket):
     for client in clients:
@@ -19,8 +21,17 @@ def handle_client(socket):
     nick = socket.recv(1024).decode()
 
     new_user = f"{nick} chegou no chat!"
-
     print(new_user)
+
+    try: #aqui vai enviar o historico pra o novo client
+        socket.end("\n--- Histórico das últimas 10 mensagens ---".encode())
+        with history_lock:
+            for msg in message_history:
+                socket.send(msg.encode())
+        socket.send("--------------------------------------------------\n".encode())
+    except:
+        pass #ignora o betinha se desconectar aqui
+    
     send_message(new_user.encode(), socket)
 
     while True:
@@ -28,14 +39,29 @@ def handle_client(socket):
             message = socket.recv(1024)
             if not message:
                 break
+
+            formatted_message = f"({nick}): {message.decode()}" #na hora do send_message
+
+            with histoy_lock: #adiciona msg nova ao historico
+                message_history.append(formatted_message)
+                if len(message_history) > 10: #aqui garante o max de 10 msg
+                    message_history.pop(0) #remove a mais antiga
             
-            send_message(f"({nick}): {message.decode()}".encode(), socket)
+            send_message(formatted_message.encode(), socket) #papo de clean code foi mal
         except:
             break
 
     clients.remove(socket)
     socket.close()
-    send_message(f"{nick} DESCONECTOU".encode(), socket)
+
+    disconnect_msg = f"{nick} DESCONECTOU"
+
+    with history_lock: #aqui tbm pra adicionar a mensagem de desconnect no historico
+        message_history.append(disconnect_msg)
+        if len(message_history) > 10:
+            message_history.pop(0)
+    
+    send_message(disconnect_msg.encode(), socket)
     print(f"Tchau {nick}! Foi bom te ver aqui!")
 
 def start_server():
